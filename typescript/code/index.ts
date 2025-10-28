5b4f6726e2500494cb03fffcb86a64c78bd3b17c
YH <1191759350@qq.com>
2025-10-28 18:00:43 +0800

    vault backup: 2025-10-28 18:00:43

---
+++ typescript/code/index.ts
@@ -0,0 +1,7 @@
+const pos: number = 0;
+
+function greet(person: string, date: Date) {
+  console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+}
+
+greet("Maddison", new Date());
---
+++ typescript/tsc.md
@@ -0,0 +1,8 @@
+typescript编译器
+
+##### 安装
+npm install -g typescript
+
+##### 功能
+1. 将ts代码转换为js代码
+2. 重写代码语法，比如es6代码转换到es3，默认转到es3

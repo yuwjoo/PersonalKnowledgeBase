9d7498e2d3afc6ccdf167e111b9fcce1eb0cae25
YH <1191759350@qq.com>
2025-10-31 18:00:15 +0800

    vault backup: 2025-10-31 18:00:15

--- typescript/code/index.ts
+++ typescript/code/index.ts
@@ -1,10 +1,10 @@
 const pos: number = 0;
 
-interface Window {
-  title: string;
+interface Window<T> {
+  title: T;
 }
 // 可以向现有接口追加字段，但type不能
-interface Window {
+interface Window<T> {
   ts: number;
 }
 
@@ -30,3 +30,52 @@ type Bird = { cc: boolean };
 function isFish(pet: Fish): boolean {
   return (pet as Fish) !== undefined;
 }
+
+type SomeConstructor = {
+  new (s: string): void;
+  new (b: number, a: string): void;
+};
+
+const aaFun: SomeConstructor;
+new aaFun(2, "");
+
+type User<T> = Array<T>;
+
+interface SquareConfig {
+  color?: string;
+  width?: number;
+}
+
+function createSquare(config: SquareConfig): { color: string; area: number } {
+  return {
+    color: config.color || "red",
+    area: config.width ? config.width * config.width : 20,
+  };
+}
+
+const obj = { colour: "red", width: 100 };
+
+let mySquare = createSquare(obj);
+
+interface Person1 {
+  name: string;
+}
+
+interface Person2 {
+  name: number;
+}
+
+type Staff = Person1 & Person2;
+
+declare const staffer: Staff;
+staffer.name;
+
+interface MakeDate {
+  (timestamp: number): Date;
+  (m: number, d: number, y: number): Date;
+}
+const makeDate: MakeDate = (m: number, d?: number, y?: number) => {
+  return new Date();
+};
+
+makeDate(22);
